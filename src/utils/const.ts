0a7102e6ef0c106f098ff7ed586d340e3f0ba8ba
Bennette321321 <6631321321@student.chula.ac.th>
2025-10-08 23:16:13 +0700

    chore: update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const SERVER_IP = "192.168.1.65";
+const BASE_URL = `http://${SERVER_IP}:3000`;
 export const BASE_API_PATH = `${BASE_URL}/api`;
 export const BASE_SOCKET_PATH = `${BASE_URL}`;
