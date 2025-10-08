ca2cee59e37e83159be726a91a4ff71ed2f0aa44
BadLuckZ <thanagorn8802@gmail.com>
2025-10-09 00:11:30 +0700

    chore: update vite

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,6 @@
-const SERVER_IP = "192.168.1.65";
+const SERVER_IP = "172.20.10.3";
+// YuTThaNa's Hotspot and YuTThaNa's Device as a server
+
 const BASE_URL = `http://${SERVER_IP}:3000`;
 export const BASE_API_PATH = `${BASE_URL}/api`;
 export const BASE_SOCKET_PATH = `${BASE_URL}`;
